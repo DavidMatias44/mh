9cec4281ccb388537b627d808cb65418487f9372
DavidMatias44 <daosmaru3667@gmail.com>
2025-04-08 08:47:47 -0600

    Ya funciona para cuatro reinas xd.

--- tabu-search.py
+++ tabu-search.py
@@ -3,22 +3,26 @@ import math
 
 
 NUM_QUEENS = 4
+MAX_TABU_LIST_LEN = 3
 
 
-def get_neighbor(x):
-    res = x.copy()
-    index1 = random.randint(0, NUM_QUEENS-1)
-    index2 = random.randint(0, NUM_QUEENS-1)
-
-    if index1 != index2:
-        res[index1], res[index2] = res[index2], res[index1]
-        return res
-
-    return get_neighbor(x)
+def get_neighbors(x):
+    neighbors = []
+    for i in range(NUM_QUEENS):
+        for j in range(i + 1, NUM_QUEENS):
+            neighbor = x.copy()
+            neighbor[i], neighbor[j] = neighbor[j], neighbor[i]
+            neighbors.append(neighbor)
+    return neighbors
     
 
-def get_total_collisions():
-    pass
+def get_total_collisions(x):
+    colissions = 0
+    for i in range(NUM_QUEENS):
+        for j in range(i + 1, NUM_QUEENS):
+            if abs(i - j) == abs(x[i] - x[j]):
+                colissions += 1
+    return colissions
 
 
 def tabu_search(s0):
@@ -26,18 +30,42 @@ def tabu_search(s0):
     best_candidate = s0
 
     tabu_list = []
-    tabu_list.append()
-    # while 
-    pass
+    tabu_list.append(s0)
+
+    # TEST
+    # stop = True
+    i = 1000
+    while i > 0:
+        neighbors = get_neighbors(best_candidate)
+        best_candidate_fitness = math.inf
+
+        for neighbor in neighbors:
+            neighbor_fitness = get_total_collisions(neighbor)
+            if neighbor not in tabu_list and neighbor_fitness < best_candidate_fitness:
+                best_candidate = neighbor
+                best_candidate_fitness = neighbor_fitness
+        
+        if best_candidate_fitness == math.inf: break;
+
+        if best_candidate_fitness < get_total_collisions(best_solution):
+            best_solution = best_candidate
+            
+        tabu_list.append(best_candidate)
+        if len(tabu_list) > MAX_TABU_LIST_LEN:
+            del tabu_list[0]
+
+        i -= 1
+
+    return best_solution
 
 
 def main():
+    # TODO: implementar un forma de generar una solucion aleatoria xd.
+    # a = [4, 3, 2, 1]
     a = [1, 2, 3, 4]
-    for i in range(NUM_QUEENS):
-        for j in range(NUM_QUEENS):
-            # if 
-            pass
 
+    res = tabu_search(a)
+    print(f"Solución: {res} -> colisiones: {get_total_collisions(res)}")
 
 
 main()
